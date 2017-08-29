e95e0a48c74c76a7ccb0b59386e735f10498e6e4
Matt Garner <matt@mocavo.com>
2017-08-28 19:54:04 -0600

    adjust sql mode configurability for mysql connector

--- src/Illuminate/Database/Connectors/MySqlConnector.php
+++ src/Illuminate/Database/Connectors/MySqlConnector.php
@@ -36,17 +36,7 @@ class MySqlConnector extends Connector implements ConnectorInterface {
 
 		$connection->prepare($names)->execute();
 
-		// If the "strict" option has been configured for the connection we'll enable
-		// strict mode on all of these tables. This enforces some extra rules when
-		// using the MySQL database system and is a quicker way to enforce them.
-		if (isset($config['strict']) && $config['strict'])
-		{
-			$connection->prepare("set session sql_mode='STRICT_ALL_TABLES'")->execute();
-		}
-		else
-		{
-			$connection->prepare("set session sql_mode=''")->execute();
-		}
+		$this->setModes($connection, $config);
 
 		return $connection;
 	}
@@ -101,5 +91,26 @@ class MySqlConnector extends Connector implements ConnectorInterface {
                         ? "mysql:host={$host};port={$port};dbname={$database}"
                         : "mysql:host={$host};dbname={$database}";
 	}
+
+	  /**
+     * Set the modes for the connection.
+     *
+     * @param  \PDO  $connection
+     * @param  array  $config
+     * @return void
+     */
+    protected function setModes(PDO $connection, array $config)
+    {
+        if (isset($config['modes'])) {
+            $modes = implode(',', $config['modes']);
+            $connection->prepare("set session sql_mode='".$modes."'")->execute();
+        } elseif (isset($config['strict'])) {
+            if ($config['strict']) {
+                $connection->prepare("set session sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'")->execute();
+            } else {
+                $connection->prepare("set session sql_mode='NO_ENGINE_SUBSTITUTION'")->execute();
+            }
+        }
+    }
 
 }
